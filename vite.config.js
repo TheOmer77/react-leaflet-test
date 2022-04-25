208e77cd0c6eb79005c8a59bd0ac6900682025a8
Omer <27678413+TheOmer77@users.noreply.github.com>
2022-04-25 18:46:29 +0300

    Replace @nabla/vite-plugin-eslint with vite-plugin-linter

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,17 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import eslint from '@nabla/vite-plugin-eslint';
+import { linterPlugin, EsLinter } from 'vite-plugin-linter';
 import svgr from 'vite-plugin-svgr';
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react(), eslint(), svgr({ svgrOptions: { icon: true } })],
+export default defineConfig((configEnv) => ({
+  plugins: [
+    react(),
+    linterPlugin({
+      include: ['./src/**/*.js', './src/**/*.jsx'],
+      linters: [new EsLinter({ configEnv })],
+    }),
+    svgr({ svgrOptions: { icon: true } }),
+  ],
   server: { port: 4000, host: true },
-});
+}));
