8475d2a1d2aaa11b47f0d6c1971b75abe890c7da
Omer <27678413+TheOmer77@users.noreply.github.com>
2022-10-22 22:27:22 +0300

    Replace `vite-plugin-linter` with `vite-plugin checker`

--- vite.config.js
+++ vite.config.js
@@ -1,17 +1,19 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import { linterPlugin, EsLinter } from 'vite-plugin-linter';
+import { checker } from 'vite-plugin-checker';
 import svgr from 'vite-plugin-svgr';
 
 // https://vitejs.dev/config/
-export default defineConfig((configEnv) => ({
+export default defineConfig({
   plugins: [
     react(),
-    linterPlugin({
-      include: ['./src/**/*.js', './src/**/*.jsx'],
-      linters: [new EsLinter({ configEnv })],
+    checker({
+      typescript: true,
+      eslint: {
+        lintCommand: 'eslint "./src/**/*.jsx"',
+      },
     }),
     svgr({ svgrOptions: { icon: true } }),
   ],
   server: { port: 4000, host: true },
-}));
+});
